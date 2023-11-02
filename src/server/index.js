6a6bdbcd7775fdfc829415a3604a46f8cd8b3836
Felipe <u202110902@upc.edu.pe>
2023-11-02 00:23:39 -0500

    add model and serverside

---
+++ src/server/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express()
+const PORT = 8000
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About route 🎉 ')
+})
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+})
