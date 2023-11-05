de11b1fe88f31163000416ce86fa5a428807edcd
Felipe <u202110902@upc.edu.pe>
2023-11-05 01:48:57 -0500

    fix bugs

---
+++ src/server/.puppeteerrc.cjs
@@ -0,0 +1,4 @@
+const {join} = require('path');
+module.exports = {
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer')
+};
--- src/server/scrap/scrap.js
+++ src/server/scrap/scrap.js
@@ -4,7 +4,10 @@ import { PAGES_NEWS } from '../consts/const.js'
 const cacheLinks = new Map()
 
 async function scrapeLinks (url, keyword) {
-  const browser = await puppeteer.launch({ headless: 'new' })
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: ['--no-sandbox']
+  })
   const page = await browser.newPage()
   await page.goto(url)
 
