c1960b6116f6714ec279328ccea9441c9d3dab97
Felipe <u202110902@upc.edu.pe>
2023-11-04 23:27:38 -0500

    cache request

--- src/server/index.js
+++ src/server/index.js
@@ -2,26 +2,40 @@
 import { extract } from '@extractus/article-extractor'
 import express from 'express'
 import cors from 'cors'
+import * as tf from '@tensorflow/tfjs'
 
 const app = express()
 const PORT = 8000
 
+// CONST CACHE
+let cacheRequest = new Map();
+
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('APIS for Web Scrapping!!!')
 })
 
-app.get('/ext', async (req, res) => {
-  const query = req.query.url
-  const decodedValue = decodeURIComponent(query)
-  const items = decodedValue.split(',')
-  const news = []
-  for (let item of items) {
+app.post('/ext', async (req, res) => {
+  const query = req.body
+  const items = query.links
+
+  const promises = items.map(async item => {
+    if (cacheRequest.has(item)) return cacheRequest.get(item)
+		console.log(`creating item ${item}`)
     const _new = await extract(item)
-    news.push(_new)
-  }
-  res.json(news)
+    const body = {
+      title: _new.title,
+      image: _new.image,
+      url: _new.url
+    }
+    cacheRequest.set(item,body)
+    return body
+  })
+
+  const news = await Promise.all(promises)
+  res.status(201).json(news)
 })
 
 app.listen(PORT, '0.0.0.0', () => {
